7469f5d6403ce995dfa04f8d191108aa5aa9d155
LidiiaArr <100479453+LidiiaArr@users.noreply.github.com>
2022-03-09 14:44:05 +0300

    Add files via upload

---
+++ 2 app.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
+}
+
+function AppTitle() {
+    return <>This is APP component</>
+
+}
+
+function Rating() {
+    debugger
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Star() {
+    return (
+        <div>star</div>
+    )
+}
+
+function Accordion() {
+    return <div>
+        <h3>Меню</h3>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+}
+
+
+export default App;
+
